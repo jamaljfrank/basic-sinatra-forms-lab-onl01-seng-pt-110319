588b53f53103684db64fc42b64141fcaeb55800e
Jamal Frank <jamaljfrank@gmail.com>
2020-02-18 04:33:08 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ class App < Sinatra::Base
   end
   
   post '/team' do
-    "Team name: Bballers"
+    @name = params[:name]
     @coach = params[:coach]
     @point_guard = params[:pg]
     @shooting_guard = params[:sg]
